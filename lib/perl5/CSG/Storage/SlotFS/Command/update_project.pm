37a9f967cc444c2ee7584a056eba7f316ed301d8
Chris Scheller <schelcj@pobox.com>
2016-04-25 08:58:51 -0400

    working now

--- lib/perl5/CSG/Storage/SlotFS/Command/update_project.pm
+++ lib/perl5/CSG/Storage/SlotFS/Command/update_project.pm
@@ -2,16 +2,37 @@ package CSG::Storage::SlotFS::Command::update_project;
 
 use CSG::Storage::SlotFS -command;
 
+use CSG::Storage::Slots::DB;
+
 sub opt_spec {
-  return ();
+  return (
+    ['project|p=s', 'Project to modifiy', {required => 1}],
+    ['name=s',      'New project name',   {requried => 1}]
+  );
 }
 
 sub validate_args {
   my ($self, $opts, $args) = @_;
+
+  my $schema = CSG::Storage::Slots::DB->new();
+  my $project = $schema->resultset('Project')->find({name => $opts->{project}});
+  unless ($project) {
+    $self->usage_error('Project does not exist');
+  }
+
+  if ($schema->resultset('Project')->find({name => $opts->{name}})) {
+    $self->usage_error('New project name already exists');
+  }
+
+
 }
 
 sub execute {
   my ($self, $opts, $args) = @_;
+
+  my $schema = CSG::Storage::Slots::DB->new();
+  my $project = $schema->resultset('Project')->find({name => $opts->{project}});
+  $project->update({name => $opts->{name}});
 }
 
 1;
