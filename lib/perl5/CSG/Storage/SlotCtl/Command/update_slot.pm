3f00417915fcf3601cad3ccaa9406dac46952e7e
Chris Scheller <schelcj@pobox.com>
2016-04-26 08:49:42 -0400

    allow for updating an existing slots size but make sure we do not exceed the available space in the pool

--- lib/perl5/CSG/Storage/SlotCtl/Command/update_slot.pm
+++ lib/perl5/CSG/Storage/SlotCtl/Command/update_slot.pm
@@ -4,26 +4,39 @@ use CSG::Storage::SlotCtl -command;
 use CSG::Storage::Slots::DB;
 use CSG::Base;
 
+use Number::Bytes::Human qw(parse_bytes);
+
 my $schema = CSG::Storage::Slots::DB->new();
 
 sub opt_spec {
   return (
-    ['id=i', 'Slot id to modifiy', {required => 1}],
+    ['id=i',   'Slot id to modifiy',                                          {required => 1}],
+    ['size=s', 'New size for the slot in human readable format (e.g. 200GB)', {required => 1}],
   );
 }
 
 sub validate_args {
   my ($self, $opts, $args) = @_;
+
+  unless ($schema->resultset('Slot')->find($opts->{id})) {
+    $self->usage_error('Unable to locate slot');
+  }
 }
 
 sub execute {
   my ($self, $opts, $args) = @_;
 
-  # TODO - allow the size to be changed but nothing else
-  #        if the size does change then need to apply the
-  #        slot allocation logic to make sure we don't
-  #        go over the available space. if we do bail out
-  #        without making changes.
+  my $size = parse_bytes($opts->{size});
+  my $slot = $schema->resultset('Slot')->find($opts->{id});
+
+  if ($size > $slot->size) {
+    unless ($slot->pool->is_available($size - $slot->size)) {
+      say 'Pool does not have enough available space';
+      exit 1;
+    }
+  }
+
+  $slot->update({size => $size});
 }
 
 1;
@@ -32,4 +45,4 @@ __END__
 
 =head1
 
-CSG::Storage::SlotCtl::Command::update_slot - Update an existing slot
+CSG::Storage::SlotCtl::Command::update_slot - Update the size of an existing slot
