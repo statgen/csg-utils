f5e0555600ff8e0928f7d30b8847e2466a444fe8
Chris Scheller <schelcj@pobox.com>
2016-05-12 08:49:13 -0400

    trying to setup cluster detection but failing

--- lib/perl5/CSG/Mapper.pm
+++ lib/perl5/CSG/Mapper.pm
@@ -2,15 +2,35 @@ package CSG::Mapper;
 
 use App::Cmd::Setup -app;
 
+use CSG::Base;
+use CSG::Mapper::Util qw(detect_cluster);
+
 sub global_opt_spec {
   return (
+    ['help|h',    'Usage'],
     ['debug|d',   'Debug output'],
     ['verbose|v', 'Verbose output'],
     ['dry-run|n', 'Dry run; show what would be done without actaully doing anything'],
-    ['help|h',    'Usage'],
-    ['cluster=s', 'Cluster environment (valid clusters: csg|flux)'],
-    ['project=s', 'Project settings to load'],
+    ['cluster=s', 'Cluster environment (valid clusters: csg|flux)', {required => 1}],
+    ['project=s', 'Project settings to load',                       {required => 1}],
   );
+}
+
+sub prepare_args {
+  my ($self) = @_;
+
+  if (my $cluster = detect_cluster()) {
+    unless (grep {/\-\-cluster/} @ARGV) {
+      push @ARGV, ('--cluster', $cluster);
+    }
+  }
+
+  return (@ARGV);
+}
+
+sub validate_args {
+  my ($self, $opt, $args) = @_;
+  die;
 }
 
 1;
