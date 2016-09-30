15bddb02df5d1b57e12aca07b63d6d55695dd448
Chris Scheller <schelcj@pobox.com>
2016-09-30 08:09:57 -0400

    tying up loose ends but this isn't used

--- lib/perl5/CSG/Mapper/DB/Schema/ResultSet/Sample.pm
+++ lib/perl5/CSG/Mapper/DB/Schema/ResultSet/Sample.pm
@@ -4,32 +4,9 @@ use base qw(DBIx::Class::ResultSet);
 use CSG::Base;
 use CSG::Mapper::Exceptions;
 
-sub ready_for_alignment {
-  my ($self, $build) = @_;
-
-  return $self->search(
-    {
-      'step.name'           => 'bam2fastq',
-      'state.name'          => 'submitted',
-      'results.build'       => $build,
-      'results.exported_at' => undef,
-      'jobs.submitted_at'   => {'!=' => undef},
-      'jobs.started_at'     => {'!=' => undef},
-      'jobs.ended_at'       => {'!=' => undef},
-      'jobs.exit_code'      => 0,
-    }, {
-      join => {'results' => ['state', {'jobs' => 'step'}]},
-      '+select' => ['results.id', 'jobs.id'],
-      '+as'     => [qw(result_id job_id)],
-    }
-  );
-}
-
 sub available_for {
   my ($self, $build, $step)  = @_;
 
-  # TODO - this still isn't quite right in some cases
-  #
   my $meth = qq{_available_for_$step};
   unless ($self->can($meth)) {
     CSG::Mapper::Exceptions::Sample::InvalidStep->throw('invalid availability');
@@ -39,11 +16,16 @@ sub available_for {
 }
 
 sub _available_for_bam2fastq {
+  # TODO - needs to find samples where there is no result and where a result has
+  #        is requested
   return shift->search(
     {
-      'results.id' => undef,
+      -or => [
+        {'results.id'  => undef},
+        {'state.name' => 'requested'},
+      ],
     }, {
-      join     => 'results',
+      join     => {results => {results_states_steps => [qw(state step)]}},
       order_by => 'RAND()',
     }
   );
