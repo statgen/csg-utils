aa85192188e621936d20300a66fbc798860b0571
Chris Scheller <schelcj@pobox.com>
2016-12-07 11:29:10 -0500

    got the schema right

--- lib/perl5/CSG/Mapper/DB/Schema/Result/Step.pm
+++ lib/perl5/CSG/Mapper/DB/Schema/Result/Step.pm
@@ -53,7 +53,7 @@ __PACKAGE__->table("steps");
 =head2 parent_id
 
   data_type: 'integer'
-  default_value: 0
+  is_foreign_key: 1
   is_nullable: 1
 
 =cut
@@ -69,7 +69,7 @@ __PACKAGE__->add_columns(
     size => 45,
   },
   "parent_id",
-  { data_type => "integer", default_value => 0, is_nullable => 1 },
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
 );
 
 =head1 PRIMARY KEY
@@ -100,6 +100,26 @@ __PACKAGE__->add_unique_constraint("name_UNIQUE", ["name"]);
 
 =head1 RELATIONS
 
+=head2 parent
+
+Type: belongs_to
+
+Related object: L<CSG::Mapper::DB::Schema::Result::Step>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "parent",
+  "CSG::Mapper::DB::Schema::Result::Step",
+  { id => "parent_id" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "NO ACTION",
+    on_update     => "NO ACTION",
+  },
+);
+
 =head2 results_states_steps
 
 Type: has_many
@@ -115,9 +135,24 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 steps
 
-# Created by DBIx::Class::Schema::Loader v0.07045 @ 2016-12-06 15:34:03
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:8SHB1FVGFK+xHeR/LljYng
+Type: has_many
+
+Related object: L<CSG::Mapper::DB::Schema::Result::Step>
+
+=cut
+
+__PACKAGE__->has_many(
+  "steps",
+  "CSG::Mapper::DB::Schema::Result::Step",
+  { "foreign.parent_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07045 @ 2016-12-07 11:27:12
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:f/A2BMEjqLWTeWeQew/OFA
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
