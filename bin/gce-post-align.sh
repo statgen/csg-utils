f280e84abdabc1968e63790981110b706b72e06e
jonathonl <jonathonl@users.noreply.github.com>
2016-12-09 10:57:35 -0500

    Create gce-post-align.sh

---
+++ bin/gce-post-align.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -o pipefail
+
+ZONES[0]="us-central1-a"
+ZONES[1]="us-central1-b"
+ZONES[2]="us-central1-c"
+ZONES[3]="us-central1-f"
+MACHINE_ZONE=${ZONES[$[ $RANDOM % 4 ]]}
+MACHINE_NAME=$(echo "post-align-"$(basename $1) | tr "[:upper:]" "[:lower:]" | sed "s/[^a-z0-9]/-/g" | head -c62)
+#MACHINE_TYPE_OPTS="--machine-type n1-standard-2"
+MACHINE_TYPE_OPTS="--custom-cpu 1 --custom-memory 6656MiB"
+
+gcloud compute instances create --zone $MACHINE_ZONE --scopes storage-full --image ubuntu-1404-alignment $MACHINE_TYPE_OPTS --boot-disk-size 300 $MACHINE_NAME
+
+EXIT_STATUS=$?
+if [[ $EXIT_STATUS == 0 ]]
+then
+  # Give ssh daemon some time to get running.
+  sleep 30s  
+
+  echo "[$(date)] Downloading input crams"
+  START_TIME=$(date +%s)
+  gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- gsutil cp $1'/*.cram' /home/alignment/
+  EXIT_STATUS=$?
+  echo "[$(date)] Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+
+  if [[ $EXIT_STATUS == 0 ]]
+  then
+    REMOTE_COMMAND="set -euo pipefail; for INPUT_FILE in /home/alignment/*.cram; do samtools sort --reference /home/alignment/ref/hs38DH.fa --threads 1 -T /home/alignment/tmp -o \\\${INPUT_FILE%.cram}.sorted.bam \\\$INPUT_FILE; rm \\\$INPUT_FILE; done; samtools merge --threads 1 /home/alignment/merged.bam /home/alignment/*.sorted.bam; rm /home/alignment/*.sorted.bam; bam-non-primary-dedup dedup_LowMem --allReadNames --binCustom --binQualS 0:2,3:3,4:4,5:5,6:6,7:10,13:20,23:30 --log /home/alignment/dedup_lowmem.metrics --recab --in /home/alignment/merged.bam --out -.ubam --refFile /home/alignment/ref/hs38DH.fa --dbsnp /home/alignment/dbsnp/Homo_sapiens_assembly38.dbsnp138.vcf.gz | samtools view -h -C -T /home/alignment/ref/hs38DH.fa -o /home/alignment/output.cram --threads 1"
+
+    echo "[$(date)] Creating container"
+    CONTAINER_ID=$(gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker create -v "/home/alignment:/home/alignment" statgen/alignment /bin/bash -c \""$REMOTE_COMMAND"\")
+
+    echo "[$(date)] Starting container"
+    gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker start $CONTAINER_ID
+
+    CONTAINER_IS_RUNNING=1
+    FAILED_CONTAINER_POLL_COUNT=0
+    while [[ $CONTAINER_IS_RUNNING != 0 && $FAILED_CONTAINER_POLL_COUNT -lt 5 ]]
+    do
+      sleep 180s
+
+      CONTAINER_STATUS=$(gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker ps -al --format {{.Status}})
+      if [[ $CONTAINER_STATUS =~ Exited\ \((.*)\) ]]
+      then
+        CONTAINER_IS_RUNNING=0
+        EXIT_STATUS=${BASH_REMATCH[1]}
+
+        echo "[$(date)] Fetching logs"
+        gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME --command "sudo docker logs $CONTAINER_ID"
+        if [[ $? != 0 ]]
+        then
+          echo "[$(date)] Fetching logs FAILED!"
+        fi
+      fi
+
+      if [[ $CONTAINER_STATUS ]]
+      then
+        FAILED_CONTAINER_POLL_COUNT=0
+      else
+        let FAILED_CONTAINER_POLL_COUNT++
+        echo "[$(date)] Failed Count: $FAILED_CONTAINER_POLL_COUNT"
+      fi
+    done
+
+    if [[ $CONTAINER_IS_RUNNING == 0 && $EXIT_STATUS == 0 ]]
+    then
+      OUTPUT_FILE=$2
+      echo "[$(date)] Uploading $OUTPUT_FILE"
+      START_TIME=$(date +%s)
+      gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp /home/alignment/output.cram $OUTPUT_FILE
+      EXIT_STATUS=$?
+      echo "[$(date)] Upload exit status: $EXIT_STATUS"
+      echo "[$(date)] Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+    elif [[ $FAILED_CONTAINER_POLL_COUNT == 5 && $RETRY_COUNTER -lt 4 ]]
+    then
+      echo "[$(date)] Lost communication with $MACHINE_NAME"
+    fi
+  fi
+fi
+
+# Machine may exist even if "docker-machine create" fails.
+echo "[$(date)] Cleaning up ..."
+gcloud compute instances delete --zone $MACHINE_ZONE --quiet $MACHINE_NAME
+
+exit $EXIT_STATUS
