cb39967962c5a416e9b2e59a8cfec0dcf053d27f
jonathonl <jonathonl@users.noreply.github.com>
2016-12-09 15:44:19 -0500

    Adds better retry logic and zone distribution.

--- bin/gce-align.sh
+++ bin/gce-align.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -o pipefail
+
 PAIRED=$1
 shift
 SAMPLE_ID=$1
@@ -10,108 +12,124 @@ shift
 OUTPUT_DIR=$1
 shift
 INPUT_FILES=$@
-
-set -o pipefail
+INPUT_FILES_COUNT=$#
 
 case "$PAIRED" in
   0) BWA_PAIRED_OPT="" ;;
   1) BWA_PAIRED_OPT="-p" ;;
 esac
 
-MACHINE_NAME="align-${SAMPLE_ID}-${RG_IDENT}"
+ZONES[0]="us-central1-b"
+ZONES[1]="us-central1-c"
+ZONES[2]="us-central1-f"
+MACHINE_ZONE=${ZONES[$[ $RANDOM % 3 ]]}
+
+MACHINE_NAME=$(echo "align-${SAMPLE_ID}-${RG_IDENT}" | tr "[:upper:]" "[:lower:]" | sed "s/[^a-z0-9]/-/g" | head -c62)
 MACHINE_TAG=$(basename $1 .fastq.gz | tr "[:upper:]" "[:lower:]" | sed "s/[^a-z0-9]/-/g" | head -c62)
 MACHINE_TYPE_OPTS="--custom-cpu 32 --custom-memory 64GiB"
 #MACHINE_TYPE_OPTS="--machine-type  n1-standard-32"
 
-gcloud compute instances create --scopes storage-full --tags $MACHINE_TAG --image ubuntu-1404-alignment $MACHINE_TYPE_OPTS --boot-disk-size 300 --preemptible $MACHINE_NAME
+EXIT_STATUS=0
+RETRY_COUNTER=0
+while [[ $# -gt 0 && $EXIT_STATUS == 0 && $RETRY_COUNTER -lt 5 ]]
+do
 
-EXIT_STATUS=$?
-if [[ $EXIT_STATUS == 0 ]]
-then
-  # Give ssh daemon some time to get running.
-  sleep 30s
+	gcloud compute instances create --zone $MACHINE_ZONE --scopes storage-full --tags $MACHINE_TAG --image ubuntu-1404-alignment $MACHINE_TYPE_OPTS --boot-disk-size 300 --preemptible $MACHINE_NAME
 
-  #COMMAND="set -o pipefail; bwa mem -t 32 -K 100000000 -Y -p -R '$RG_LINE' /home/alignment/ref/hs38DH.fa /home/alignment/input.fastq.gz | samtools sort -@ 32 -m 2000M --reference /home/alignment/ref/hs38DH.fa -O cram -o /home/alignment/output.cram -T /home/alignment/sort.temp -"
-  COMMAND="set -o pipefail; rm -f /home/alignment/output.cram /home/alignment/output.cram.ok; bwa mem -t 32 -K 100000000 -Y $BWA_PAIRED_OPT -R '$RG_LINE' /home/alignment/ref/hs38DH.fa /home/alignment/input.fastq.gz | samblaster -a --addMateTags | samtools view -@ 32 -T /home/alignment/ref/hs38DH.fa -C -o /home/alignment/output.cram - && touch /home/alignment/output.cram.ok"
+	EXIT_STATUS=$?
+	if [[ $EXIT_STATUS == 0 ]]
+	then
+	  # Give ssh daemon some time to get running.
+	  sleep 30s
 
-  CONTAINER_ID=$(gcloud compute ssh $MACHINE_NAME -- sudo docker create -v "/home/alignment:/home/alignment" statgen/alignment /bin/bash -c \""$COMMAND"\")
+	  #COMMAND="set -o pipefail; bwa mem -t 32 -K 100000000 -Y -p -R '$RG_LINE' /home/alignment/ref/hs38DH.fa /home/alignment/input.fastq.gz | samtools sort -@ 32 -m 2000M --reference /home/alignment/ref/hs38DH.fa -O cram -o /home/alignment/output.cram -T /home/alignment/sort.temp -"
+	  COMMAND="set -o pipefail; rm -f /home/alignment/output.cram /home/alignment/output.cram.ok; bwa mem -t 32 -K 100000000 -Y $BWA_PAIRED_OPT -R '$RG_LINE' /home/alignment/ref/hs38DH.fa /home/alignment/input.fastq.gz | samblaster -a --addMateTags | samtools view -@ 32 -T /home/alignment/ref/hs38DH.fa -C -o /home/alignment/output.cram - && touch /home/alignment/output.cram.ok"
 
-  EXIT_STATUS=$?
-  if [[ $EXIT_STATUS == 0 ]]
-  then  
-    for f in $INPUT_FILES
-    do
-      echo "Downloading "$f" ..."
-      START_TIME=$(date +%s)
-      gcloud compute ssh $MACHINE_NAME -- gsutil cp $f /home/alignment/input.fastq.gz
-      EXIT_STATUS=$?
-      echo "Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+	  CONTAINER_ID=$(gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker create -v "/home/alignment:/home/alignment" statgen/alignment /bin/bash -c \""$COMMAND"\")
 
-      RETRY_COUNTER=0
-      while [[ $EXIT_STATUS == 0 && $RETRY_COUNTER -lt 5 ]]
-      do
+	  EXIT_STATUS=$?
+	  if [[ $EXIT_STATUS == 0 ]]
+	  then 
+	    while [[ $# -gt 0 ]]
+	    do
+	    	F=$1
+	      echo "Downloading "$F" ..."
+	      START_TIME=$(date +%s)
+	      gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- gsutil cp $F /home/alignment/input.fastq.gz
+	      EXIT_STATUS=$?
+	      echo "Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+	  
+	      gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker start $CONTAINER_ID
 
-        gcloud compute ssh $MACHINE_NAME -- sudo docker start $CONTAINER_ID
+	      CONTAINER_IS_RUNNING=1
+	      FAILED_CONTAINER_POLL_COUNT=0
+	      while [[ $CONTAINER_IS_RUNNING != 0 && $FAILED_CONTAINER_POLL_COUNT -lt 5 ]]
+	      do
+	        sleep 60s
 
-        CONTAINER_IS_RUNNING=1
-        FAILED_CONTAINER_POLL_COUNT=0
-        while [[ $CONTAINER_IS_RUNNING != 0 && $FAILED_CONTAINER_POLL_COUNT -lt 5 ]]
-        do
-          sleep 60s
+	        CONTAINER_STATUS=$(gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- sudo docker ps -al --format {{.Status}})
+	        if [[ $CONTAINER_STATUS =~ Exited\ \((.*)\) ]]
+	        then
+	          CONTAINER_IS_RUNNING=0
+	          EXIT_STATUS=${BASH_REMATCH[1]}
 
-          CONTAINER_STATUS=$(gcloud compute ssh $MACHINE_NAME -- sudo docker ps -al --format {{.Status}})
-          if [[ $CONTAINER_STATUS =~ Exited\ \((.*)\) ]]
-          then
-            CONTAINER_IS_RUNNING=0
-            EXIT_STATUS=${BASH_REMATCH[1]}
+	          echo "Fetching logs ..."
+	          gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME --command "sudo docker logs $CONTAINER_ID"
+	          if [[ $? != 0 ]]
+	          then
+	            echo "Fetching logs FAILED!"
+	          fi
+	        fi
 
-            echo "Fetching logs ..."
-            gcloud compute ssh $MACHINE_NAME --command "sudo docker logs $CONTAINER_ID"
-            if [[ $? != 0 ]]
-            then
-              echo "Fetching logs FAILED!"
-            fi
-          fi
+	        # if [[ $(gcloud compute instances list $MACHINE_NAME | grep RUNNING | wc -l) == 0 ]]
+	        # then
+	        #   MACHINE_IS_RUNNING=0
+	        # fi
+	        if [[ $CONTAINER_STATUS ]]
+	        then
+	          FAILED_CONTAINER_POLL_COUNT=0
+	        else
+	          let FAILED_CONTAINER_POLL_COUNT++
+	          echo "Failed Count: "$FAILED_CONTAINER_POLL_COUNT
+	        fi
+	      done
 
-          # if [[ $(gcloud compute instances list $MACHINE_NAME | grep RUNNING | wc -l) == 0 ]]
-          # then
-          #   MACHINE_IS_RUNNING=0
-          # fi
-          if [[ $CONTAINER_STATUS ]]
-          then
-            FAILED_CONTAINER_POLL_COUNT=0
-          else
-            let FAILED_CONTAINER_POLL_COUNT++
-            echo "Failed Count: "$FAILED_CONTAINER_POLL_COUNT
-          fi
-        done
+	      if [[ $CONTAINER_IS_RUNNING == 0 && $EXIT_STATUS == 0 ]]
+	      then
+	        OUTPUT_FILE=$OUTPUT_DIR"/"$(basename $F .fastq.gz)".cram"
+	        echo "Uploading "$OUTPUT_FILE" ..."
+	        START_TIME=$(date +%s)
+	        gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp /home/alignment/output.cram $OUTPUT_FILE && gcloud compute ssh --zone $MACHINE_ZONE $MACHINE_NAME -- gsutil cp /home/alignment/output.cram.ok $OUTPUT_FILE".ok"
+	        EXIT_STATUS=$?
+	        echo 'Upload exit status: '$EXIT_STATUS
+	        echo "Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+	        shift #pops file off of input list
 
-        if [[ $CONTAINER_IS_RUNNING == 0 && $EXIT_STATUS == 0 ]]
-        then
-          OUTPUT_FILE=$OUTPUT_DIR"/"$(basename $f .fastq.gz)".cram"
-          echo "Uploading "$OUTPUT_FILE" ..."
-          START_TIME=$(date +%s)
-          gcloud compute ssh $MACHINE_NAME -- gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp /home/alignment/output.cram $OUTPUT_FILE && gcloud compute ssh $MACHINE_NAME -- gsutil cp /home/alignment/output.cram.ok $OUTPUT_FILE".ok"
-          EXIT_STATUS=$?
-          echo 'Upload exit status: '$EXIT_STATUS
-          echo "Elapsed time: "$(( $(date +%s) - $START_TIME ))"s"
+	      elif [[ $FAILED_CONTAINER_POLL_COUNT == 5 ]]
+	      then
+	        #sleep 300s
+	        echo "Machine stopped: "$MACHINE_NAME
+	        break
+	      fi
+	    done
+	  fi
+	fi
 
-          break
-        elif [[ $FAILED_CONTAINER_POLL_COUNT == 5 && $RETRY_COUNTER -lt 4 ]]
-        then
-          #sleep 300s
-          echo "Machine stopped. Restarting "$MACHINE_NAME" ..."
-          gcloud compute instances start $MACHINE_NAME
-        fi
-
-        let RETRY_COUNTER++
-      done
-    done
+	# Machine may exist even if "docker-machine create" fails.
+	echo 'Cleaning up ...'
+	gcloud compute instances delete --zone $MACHINE_ZONE --quiet $MACHINE_NAME
+  
+  if [[ $# -gt 0 ]]
+  then
+    sleep $(( $RETRY_COUNTER * 600 ))"s"
   fi
-fi
 
-# Machine may exist even if "docker-machine create" fails.
-echo 'Cleaning up ...'
-gcloud compute instances delete --quiet $MACHINE_NAME
+  let $RETRY_COUNTER++
+done
+
+if [[ $# -gt 0 ]]
+then
+  echo "Giving up at "$1
+done
 
 exit $EXIT_STATUS
